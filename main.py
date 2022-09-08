d462c74b8358fc1134034e2b9f02169b1a496dc8
Luiz Henrique Lima da Silva <luizhenrique.limads@gmail.com>
2022-09-08 11:30:45 -0300

    test:find id:1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, jsonify, request
 import re
-
+import pymongo
 
 app = Flask(__name__)
 
@@ -10,6 +10,13 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello Worlds</h1>"
 
+@app.route("/q/<busca>",methods=['GET','POST'])
+def disp(busca):
+    client = pymongo.MongoClient("mongodb+srv://userRead:ZmlwSIDLTtMMPQFX@cluster0.nhq2ivc.mongodb.net/?retryWrites=true&w=majority")
+    Database = client.get_database('mongodb')
+    Pessoas = Database.Pessoas
+    mydoc = Pessoas.find_one({ "_id": int(busca) })
+    return mydoc
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
